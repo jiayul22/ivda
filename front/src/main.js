af92fb2d4806a863225cdb17fc2a3e4dfdb08369
keyyke <space.yokota@gmail.com>
2022-11-13 15:22:14 +0100

    add histogram

--- front/src/main.js
+++ front/src/main.js
@@ -2,6 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify'
 
+import HistogramSlider from "vue-histogram-slider";
+import "vue-histogram-slider/dist/histogram-slider.css";
+
+Vue.component(HistogramSlider.name, HistogramSlider);
+
 Vue.config.productionTip = false
 
 new Vue({
