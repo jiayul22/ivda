0548d3de03dee5400e6727f4a69e850e5a54db4b
Asysay <114915212+Asysay@users.noreply.github.com>
2022-11-16 20:50:43 +0100

    add map component

--- front/src/main.js
+++ front/src/main.js
@@ -5,6 +5,16 @@ import vuetify from './plugins/vuetify'
 import HistogramSlider from "vue-histogram-slider";
 import "vue-histogram-slider/dist/histogram-slider.css";
 
+import "leaflet/dist/leaflet.css"
+import L from 'leaflet';
+delete L.Icon.Default.prototype._getIconUrl;
+
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
+  iconUrl: require('leaflet/dist/images/marker-icon.png'),
+  shadowUrl: require('leaflet/dist/images/marker-shadow.png')
+});
+
 Vue.component(HistogramSlider.name, HistogramSlider);
 
 Vue.config.productionTip = false
