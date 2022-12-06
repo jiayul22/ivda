a1f5245b1afab53a435f525b8bd0e8518f032d4b
keyyke <space.yokota@gmail.com>
2022-12-06 23:58:28 +0100

    change histogram according to select city

--- front/src/components/histogram.vue
+++ front/src/components/histogram.vue
@@ -1,78 +1,81 @@
 <template>
   <v-card height="520">
-    <v-list-item
-      v-for="(item, i) in selectCategory"
-      :key="i"
-    >
       <v-combobox
-        :items="item.value"
-        :label=item.name
+        v-model="selectedCity"
+        :items=city
+        label="City"
         filled
         height=5
         @change="onChange($event,item.name)"
       ></v-combobox>
-    </v-list-item>
 
-    <v-row align="center" justify="center" class="mt-0 mb-0">
-      <h3>Distribution of Price</h3>
-    </v-row>
-
-    <div style="height: 50vh">
-      <div id="app">
-        <HistogramSlider
-        :width="500"
-        :bar-height="300"
-        :data="price"
-        :drag-interval="true"
-        :force-edges="true"
-        :colors="['#4facfe', '#00f2fe']"
-        :min=0
-        :max=1200
-        />
+    <div v-show="isZurich">
+      <v-row align="center" justify="center" class="mt-0 mb-0">
+        <h3>Distribution of Price</h3>
+      </v-row>
+      <div style="height: 50vh">
+        <div id="app">
+          <HistogramSlider
+          :width="500"
+          :bar-height="300"
+          :data="zurich_price"
+          :drag-interval="true"
+          :force-edges="true"
+          :colors="['#4facfe', '#00f2fe']"
+          :min=0
+          :max=1200
+          />
         </div>
+      </div>
+    </div>
+
+    <div v-show="isOslo">
+      <v-row align="center" justify="center" class="mt-0 mb-0">
+        <h3>test</h3>
+      </v-row>
+      <div style="height: 50vh">
+        <div id="app">
+          <HistogramSlider
+          :width="500"
+          :bar-height="300"
+          :data="test_price"
+          :drag-interval="true"
+          :force-edges="true"
+          :colors="['#4facfe', '#00f2fe']"
+          :min=0
+          :max=120
+          />
+        </div>
+      </div>
     </div>
 
   </v-card>
 </template>
 
 <script>
-import zurich from "./price_data/zurich.json";
+import zurich_price from "./price_data/zurich.json";
+import test from "./test_data/test_price.json";
 
 export default {
   name: "App",
 
   data:() => ({
-    price: zurich,
-    inputFeature:{
-      'City':'',
-    },
-    selectCategory:[{
-      name: 'Feature', value: ['Price', 'Room Type', 'Minimum nights']
-    }]
+    zurich_price: zurich_price,
+    test_price: test,
+    city: ['Zurich', 'Oslo', 'Berlin'],
+    selectedCity: 'Select',
   }),
 
-  methods: {
-      async sendSelectionToBackend () {
-        var paras = ''
-        for (const [key, value] of Object.entries(this.inputFeature)) {
-          console.log('输出',key, value);
-          paras = paras+key+'='+value+'&'
-        }
-        console.log("参数" + paras)
-        var reqUrl = 'http://127.0.0.1:5000/PredictPrice?'+paras
-        console.log("ReqURL " + reqUrl)
-
-        // await response and data
-        const response = await fetch(reqUrl)
-        const responseData = await response.json();
-        console.log("responseData " + responseData )
-      },
-
-      onChange(value,name){
-        //
-        console.log('输入：',name, value);
-        this.inputFeature[name] = value;
-      }
+  computed: {
+    isZurich() {
+      return this.selectedCity === 'Zurich';
+    },
+    isOslo() {
+      return this.selectedCity === 'Oslo';
+    },
+    isButton() {
+      return this.selectedCity === 3;
+    },
   },
 };
 </script>
