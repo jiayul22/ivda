c5bc2a34b273462accfb029bf74a4d21d90c948b
keyyke <space.yokota@gmail.com>
2022-12-08 01:06:52 +0100

    make bar chart to show room type

--- front/src/components/histogram.vue
+++ front/src/components/histogram.vue
@@ -8,16 +8,24 @@
         height=5
         @change="onChange($event,item.name)"
       ></v-combobox>
+      <v-combobox
+        v-model="selectedFeature"
+        :items=feature
+        label="Feature"
+        filled
+        height=5
+        @change="onChange($event,item.name)"
+      ></v-combobox>
 
-    <div v-show="isZurich">
+    <div v-show="isZurich && isP">
       <v-row align="center" justify="center" class="mt-0 mb-0">
-        <h3>Distribution of Price</h3>
+        <h3>Distribution of Price in Zurch</h3>
       </v-row>
       <div style="height: 50vh">
         <div id="app">
           <HistogramSlider
           :width="500"
-          :bar-height="300"
+          :bar-height="200"
           :data="zurich_price"
           :drag-interval="true"
           :force-edges="true"
@@ -29,6 +37,93 @@
       </div>
     </div>
 
+    <div v-show="isZurich && isP">
+      <v-row align="center" justify="center" class="mt-0 mb-0">
+        <h3>Distribution of Price in Zurch</h3>
+      </v-row>
+      <div style="height: 50vh">
+        <div id="app">
+          <HistogramSlider
+          :width="500"
+          :bar-height="200"
+          :data="zurich_price"
+          :drag-interval="true"
+          :force-edges="true"
+          :colors="['#4facfe', '#00f2fe']"
+          :min=0
+          :max=1200
+          />
+        </div>
+      </div>
+    </div>
+
+    <div v-show="isZurich && isP">
+      <v-row align="center" justify="center" class="mt-0 mb-0">
+        <h3>Distribution of Price in Zurch</h3>
+      </v-row>
+      <div style="height: 50vh">
+        <div id="app">
+          <HistogramSlider
+          :width="500"
+          :bar-height="200"
+          :data="zurich_price"
+          :drag-interval="true"
+          :force-edges="true"
+          :colors="['#4facfe', '#00f2fe']"
+          :min=0
+          :max=1200
+          />
+        </div>
+      </div>
+    </div>
+
+    <div v-show="isZurich && isP">
+      <v-row align="center" justify="center" class="mt-0 mb-0">
+        <h3>Distribution of Price in Zurch</h3>
+      </v-row>
+      <div style="height: 50vh">
+        <div id="app">
+          <HistogramSlider
+          :width="500"
+          :bar-height="200"
+          :data="zurich_price"
+          :drag-interval="true"
+          :force-edges="true"
+          :colors="['#4facfe', '#00f2fe']"
+          :min=0
+          :max=1200
+          />
+        </div>
+      </div>
+    </div>
+
+    <div v-show="isZurich && isP">
+      <v-row align="center" justify="center" class="mt-0 mb-0">
+        <h3>Distribution of Price in Zurch</h3>
+      </v-row>
+      <div style="height: 50vh">
+        <div id="app">
+          <HistogramSlider
+          :width="500"
+          :bar-height="200"
+          :data="zurich_price"
+          :drag-interval="true"
+          :force-edges="true"
+          :colors="['#4facfe', '#00f2fe']"
+          :min=0
+          :max=1200
+          />
+        </div>
+      </div>
+    </div>
+
+    <div v-show="(isZurich && isRT)">
+      <v-row align="center" justify="center" class="mt-0 mb-0">
+        <h3>Distribution of Room Type in Zurch</h3>
+      </v-row>
+      <zurich_RT/>
+    </div>
+
     <div v-show="isOslo">
       <v-row align="center" justify="center" class="mt-0 mb-0">
         <h3>test</h3>
@@ -37,7 +132,7 @@
         <div id="app">
           <HistogramSlider
           :width="500"
-          :bar-height="300"
+          :bar-height="200"
           :data="test_price"
           :drag-interval="true"
           :force-edges="true"
@@ -53,28 +148,62 @@
 </template>
 
 <script>
-import zurich_price from "./price_data/zurich.json";
+import zurich_price from "./price/zurichP.json";
 import test from "./test_data/test_price.json";
+
+import zurich_RT from './roomtypeVue/zurichRT.vue';
 
 export default {
   name: "App",
+  components: {zurich_RT},
 
   data:() => ({
     zurich_price: zurich_price,
+    zurich_RT: zurich_RT,
     test_price: test,
-    city: ['Zurich', 'Oslo', 'Berlin'],
+    city: ['Zurich', 'Berlin', 'Copenhagen', 'Oslo', 'Paris', 'Rome', 'San Francisco', 'Stockholm'],
+    feature: ['Price', 'room type', 'minimum nights'],
     selectedCity: 'Select',
+    selectedFeature: 'Select',
+
+    LinePlotData: {x: [], y: []},
+    testData: {x: ['a', 'b', 'c', 'd'], y: [110, 200, 5, 1111]},
+    responseData:{ 'profit': [{'feature': 2021, 'weight': 33364}, {'feature': 2020, 'weight': 21331}, {'feature': 2019, 'weight': 11588}]}
   }),
 
   computed: {
     isZurich() {
       return this.selectedCity === 'Zurich';
     },
+    isBerlin() {
+      return this.selectedCity === 'Berlin';
+    },
+    isCopenhagen() {
+      return this.selectedCity === 'Copenhagen';
+    },
     isOslo() {
       return this.selectedCity === 'Oslo';
     },
-    isButton() {
-      return this.selectedCity === 3;
+    isParis() {
+      return this.selectedCity === 'Paris';
+    },
+    isRome() {
+      return this.selectedCity === 'Rome';
+    },
+    isSanFrancisco() {
+      return this.selectedCity === 'San Francisco';
+    },
+    isStockholm() {
+      return this.selectedCity === 'Stockholm';
+    },
+    isP() {
+      return this.selectedFeature === 'Price';
+    },
+    isRT() {
+      return this.selectedFeature === 'room type';
+    },
+    isMN() {
+      return this.selectedFeature === 'minimum nights';
     },
   },
 };
@@ -87,12 +216,12 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  padding-top: 50px;
+  padding-top: 30px;
   margin: 0 auto;
   width: 500px;
 }
 
 .select.v-text-field.v-text-field--enclosed:not(.v-text-field--rounded)>.v-input__control>.v-input__slot {
-  padding-right: 4px
+  padding-right: 0px
 }
 </style>
---
+++ front/src/components/roomtypeVue/zurichRT.vue
@@ -0,0 +1,44 @@
+<template>
+    <main>
+        <div id='myLinePlot' style="height:350px; width:650px; margin: auto;"></div>
+    </main>
+  </template>
+
+  <script>
+  import Plotly from 'plotly.js/dist/plotly';
+
+  export default {
+    data: () => ({
+        testData: {x: ['a', 'b', 'c', 'd'], y: [110, 200, 5, 1111]},
+    }),
+
+    mounted() {
+      this.fetchData()
+    },
+
+    methods: {
+    fetchData() {
+        // draw the lineplot after the data is transformed
+        this.drawLinePlot()
+      },
+    drawLinePlot() {
+        var trace1 = {
+          x: this.testData.x,
+          y: this.testData.y,
+          type: 'bar'
+        };
+        var data = [trace1];
+        var layout = {
+          // remove the layout of title
+
+        };
+        var config = {responsive: true, displayModeBar: false}
+        Plotly.newPlot('myLinePlot', data, layout, config);
+    }
+    }
+
+  }
+  </script>
+
+  <style scoped>
+  </style>
